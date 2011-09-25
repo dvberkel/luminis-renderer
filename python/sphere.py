24e4c25ff79260ddfd93986ef7b351f7809b7a48
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-09-25 20:35:22 +0200

    Listing angles
    The angles introduces for the sphere are listed.

--- python/sphere.py
+++ python/sphere.py
@@ -1,4 +1,10 @@
 #! /usr/bin/env python
 
+import math
+
 if __name__ == '__main__':
-	print "Hello World";
+	maximum = 50
+	angle = 2 * math.pi / maximum
+	for index in range(0,maximum):
+		x = index * angle
+		print x;
