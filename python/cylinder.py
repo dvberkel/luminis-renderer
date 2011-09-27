88dd8669de5232da68cb01a4837a09e41a55735c
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-09-27 08:59:27 +0200

    Formatted points
    the points are formatted as a scene.

--- python/cylinder.py
+++ python/cylinder.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python
 
 import math
+import string
+
+sceneTemplate = "(scene [{0}])"
+pointTemplate = "({0:f}, {1:f}, {2:f}), "
 
 if __name__ == '__main__':
 	pointsPerTurn = 10
@@ -8,7 +12,11 @@ if __name__ == '__main__':
 	numberOfPoints = pointsPerTurn * numberOfTurns
 	angle = numberOfTurns * 2 * math.pi / numberOfPoints
 	dz = 2.0 / numberOfPoints
+	
+	accumulator = ""
 	for index in range(0,numberOfPoints + 1):
-		x = index * angle
+		t = index * angle
 		z = -1 + index * dz
-		print math.cos(x), math.sin(x), z;
+		accumulator += pointTemplate.format(math.cos(t), math.sin(t), z);
+		
+	print sceneTemplate.format(accumulator[0:-2])
