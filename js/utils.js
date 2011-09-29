839362def8fe410e2927b60ae14b49adf2f6187e
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-09-29 16:23:07 +0200

    Wrapped function
    The function declartion is wrapped.

--- js/utils.js
+++ js/utils.js
@@ -1,17 +1,19 @@
-window.feedback = function(msg) {
-	if ($("#feedback").is(":visible")) {
-		if (feedback.timeout) { clearTimeout(feedback.timeout); }
-	} else {
-		$("#feedback ul").empty();
+(function(){
+	window.feedback = function(msg) {
+		if ($("#feedback").is(":visible")) {
+			if (feedback.timeout) { clearTimeout(feedback.timeout); }
+		} else {
+			$("#feedback ul").empty();
+		}
+		$("#feedback ul").append("<li>" + msg + "</li>").parent().show('fast', function(){
+			feedback.timeout = setTimeout(function(){$("#feedback").hide('slow')},5000);
+		});
 	}
-	$("#feedback ul").append("<li>" + msg + "</li>").parent().show('fast', function(){
-		feedback.timeout = setTimeout(function(){$("#feedback").hide('slow')},5000);
-	});
-}
-window.time = function(callback, msg) {
-	msg = msg || "";
-	var start = new Date();
-	callback();
-	var stop = new Date();
-	console.log(msg + (stop.getTime() - start.getTime()));
-}
+	window.time = function(callback, msg) {
+		msg = msg || "";
+		var start = new Date();
+		callback();
+		var stop = new Date();
+		console.log(msg + (stop.getTime() - start.getTime()));
+	}
+})();
