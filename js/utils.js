5f1b554d9980ae24a4bd8c88d1ab13ff30ca9bab
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-09-29 16:12:20 +0200

    Utils packaged
    The utility files that are used by the core rendering is packaged§

---
+++ js/utils.js
@@ -0,0 +1,17 @@
+function feedback(msg) {
+	if ($("#feedback").is(":visible")) {
+		if (feedback.timeout) { clearTimeout(feedback.timeout); }
+	} else {
+		$("#feedback ul").empty();
+	}
+	$("#feedback ul").append("<li>" + msg + "</li>").parent().show('fast', function(){
+		feedback.timeout = setTimeout(function(){$("#feedback").hide('slow')},5000);
+	});
+}
+var time = function(callback, msg) {
+	msg = msg || "";
+	var start = new Date();
+	callback();
+	var stop = new Date();
+	console.log(msg + (stop.getTime() - start.getTime()));
+}
